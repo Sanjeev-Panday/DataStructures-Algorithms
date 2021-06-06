2f8b27b06c799c1e7d83b9e7361563b3b9121770
Sanjeev Panday <sanjeev.pandey84@gmail.com>
2021-06-06 18:02:29 +0530

    Optimal solution for Maximum water container array problem

--- src/main/java/interview/MaximumWater.java
+++ src/main/java/interview/MaximumWater.java
@@ -2,6 +2,7 @@ package interview;
 
 public class MaximumWater {
     public static int maximumWater(int[] arr) {
+        if (arr == null) return 0;
         int maxArea = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -11,4 +12,20 @@ public class MaximumWater {
         return maxArea;
     }
 
+    public static int maximumWaterV2(int[] arr) {
+        if (arr == null) return 0;
+        int maxArea = 0;
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int height = Math.min(arr[left], arr[right]);
+            int newArea = height * width;
+            maxArea = Math.max(maxArea, newArea);
+            if (arr[left] < arr[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
 }
--- src/test/java/interview/MaximumWaterTest.java
+++ src/test/java/interview/MaximumWaterTest.java
@@ -22,4 +22,28 @@ public class MaximumWaterTest {
         int[] arr = {7};
         Assert.assertEquals(0, MaximumWater.maximumWater(arr));
     }
+
+    @Test
+    public void testMaximumWaterV2() {
+        int[] arr = {7, 1, 3, 4, 9};
+        Assert.assertEquals(28, MaximumWater.maximumWaterV2(arr));
+    }
+
+    @Test
+    public void testMaximumWaterEmptyArrayV2() {
+        int[] arr = {};
+        Assert.assertEquals(0, MaximumWater.maximumWaterV2(arr));
+    }
+
+    @Test
+    public void testMaximumWaterSingleElementV2() {
+        int[] arr = {7};
+        Assert.assertEquals(0, MaximumWater.maximumWaterV2(arr));
+    }
+
+    @Test
+    public void testMaximumWaterNullArray() {
+        int[] arr = null;
+        Assert.assertEquals(0, MaximumWater.maximumWaterV2(arr));
+    }
 }
